cfcebf017077b23d2bc7c19df6deb2fb059f5b8b
Yassin El Youssfi El Yousfi <yael-you@c2r9s3.42madrid.com>
2025-02-04 17:53:43 +0100

    1753040225

--- ft_printf.c
+++ ft_printf.c
@@ -6,49 +6,54 @@
 /*   By: yael-you <yael-you@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:16:32 by yael-you          #+#    #+#             */
-/*   Updated: 2025/02/04 14:16:34 by yael-you         ###   ########.fr       */
+/*   Updated: 2025/02/04 17:38:10 by yael-you         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 
-int ft_converter(char format, va_list args)
+int	ft_converter(char format, va_list args)
 {
-    if (format == 'c')
-        ft_putchar(va_arg(args,char));
-    else if (format == 's')
-        ft_putstr(va_arg(args,char *));
-    else if (format == 'p')
-        ftptr(va_arg(args,int));
-    else if (format == 'd')
-        ft_putnbr(va_arg(args,int));
-    else if (format == 'u')
-        ft_unsigned(va_arg(args,unsigned int));
-    else if (format == 'x')
-        ft_hexa_base16_toupper(va_arg(args,int ));
-    else if (format == 'X')
-        ft_hexa_base10_tolower(va_arg(args,int));
-    else if (format == '%')
-        return ft_putchar(va_arg(args, char));
-}
-int ft_printf(char const *format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    
-    while (format)
-    {
-        if (*format == '%')
-        format ++;
-    }
-    va_end(args);
-    return (ft_strlen(format));
-}int ft_ptr()
-{
-    int i;
-    int  *ptr;
+	char	*base;
 
-    ptr = &i;
-    ft_putchar(&i);
-    
+	base = "0123456789abcdef";
+	if (format == 'c')
+		ft_putchar(va_arg(args, char));
+	else if (format == 's')
+		ft_putstr(va_arg(args, char *));
+	else if (format == 'p')
+		ftptr(va_arg(args, int));
+	else if (format == 'd')
+		ft_putnbr(va_arg(args, int));
+	else if (format == 'u')
+		ft_putnbr_base(va_arg(args, unsigned int));
+	else if (format == 'x')
+		ft_putnbr_base(va_arg(args, int));
+	else if (format == 'X')
+		ft_hexa_base10_tolower(va_arg(args, int));
+	else if (format == '%')
+		return (ft_putchar(va_arg(args, char)));
+}
+int	ft_printf(char const *format, ...)
+{
+	va_list	args;
+
+	va_start(args, format);
+	if (format == '\0')
+		 ft_putchar(' ');
+	while (format)
+	{
+		if (*format == '%')
+			format++;
+	}
+	va_end(args);
+	return (ft_strlen(format));
+}
+int	ft_ptr(void)
+{
+	int i;
+	int *ptr;
+
+	ptr = &i;
+	ft_putchar(&i);
 }
--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yael-you <yael-you@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:31:12 by yael-you          #+#    #+#             */
-/*   Updated: 2025/02/04 13:54:09 by yael-you         ###   ########.fr       */
+/*   Updated: 2025/02/04 17:30:26 by yael-you         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-void	ft_putnbr(int n)
+void	ft_putnbr_base(int n, char *base)
 {
-	
-	if (n == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
 	if (n == 0)
 	{
 		ft_putchar('0');
@@ -29,7 +23,15 @@ void	ft_putnbr(int n)
 		ft_putchar('-');
 		n = -n;
 	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	ft_putchar((n % 10) + '0');
+	if (n >= 16)
+		ft_putnbr_base(n / 16, base);
+	ft_putchar(*(base + (n % 16)));
+}
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+int main ()
+{
+	ft_putnbr_base(255,"0123456789abcdef");
 }
