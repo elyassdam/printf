b3b76472cf200e7bc9de43750c5666b38bb92fd7
Yassin El Youssfi El Yousfi <yael-you@c2r9s3.42madrid.com>
2025-02-04 15:15:58 +0100

    4021515

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yael-you <yael-you@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:16:32 by yael-you          #+#    #+#             */
-/*   Updated: 2025/02/04 02:22:44 by yael-you         ###   ########.fr       */
+/*   Updated: 2025/02/04 14:16:34 by yael-you         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,21 @@
 int ft_converter(char format, va_list args)
 {
     if (format == 'c')
-        ft_putchar()
+        ft_putchar(va_arg(args,char));
     else if (format == 's')
-        ft_putstr(format,va_list args);
+        ft_putstr(va_arg(args,char *));
     else if (format == 'p')
-        ftptr()
+        ftptr(va_arg(args,int));
     else if (format == 'd')
         ft_putnbr(va_arg(args,int));
     else if (format == 'u')
+        ft_unsigned(va_arg(args,unsigned int));
     else if (format == 'x')
+        ft_hexa_base16_toupper(va_arg(args,int ));
     else if (format == 'X')
+        ft_hexa_base10_tolower(va_arg(args,int));
     else if (format == '%')
-        return ft_putchar(va_arg(format, int));
+        return ft_putchar(va_arg(args, char));
 }
 int ft_printf(char const *format, ...)
 {
@@ -40,4 +43,12 @@ int ft_printf(char const *format, ...)
     }
     va_end(args);
     return (ft_strlen(format));
+}int ft_ptr()
+{
+    int i;
+    int  *ptr;
+
+    ptr = &i;
+    ft_putchar(&i);
+    
 }
